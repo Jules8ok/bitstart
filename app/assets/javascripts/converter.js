361297979c20d96e0808cecb2c548fc2507e1b42
Jules Wittock <wittockjules@gmail.com>
2017-11-15 18:13:21 +0100

    yo

---
+++ app/assets/javascripts/converter.js
@@ -0,0 +1,34 @@
+var px = document.querySelector('.px-input');
+var em = document.querySelector('.em-input');
+var bf = document.querySelector('.chosen');
+var pxValue = px.value;
+var basefont = bf.value;
+px.addEventListener('keyup', function() {
+  basefont = bf.value;
+  pxValue = px.value;
+  calculatePXtoEM(pxValue, basefont);
+});
+
+em.addEventListener('keyup', function() {
+  basefont = bf.value;
+  emValue = em.value;
+  calculateEMtoPX(emValue, basefont);
+});
+
+function calculatePXtoEM(givenPx, basefont) {
+  console.log(givenPx);
+  if (basefont === '') {
+    basefont = 16;
+  }
+  var returnEm = (givenPx / basefont);
+  em.value = returnEm;
+}
+
+function calculateEMtoPX(givenEm, basefont) {
+  console.log(givenEm);
+  if (basefont === '') {
+    basefont = 16;
+  }
+  var returnPx = (givenEm * basefont);
+  px.value = returnPx;
+}
--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -12,4 +12,7 @@ class PagesController < ApplicationController
 
   def sources
   end
+
+  def converter
+  end
 end
